6f4c1d36248125298126be14a853cbd9fe2d2c0e
Vasyl <vasyl127@gmail.com>
2021-10-25 21:35:25 +0300

    delete test file

--- application.rb
+++ application.rb
@@ -2,3 +2,17 @@ require_relative "car"
 require_relative "model/car_model"
 require_relative "view/view"
 require_relative "drag"
+
+#initialize
+
+car_model1 = CarModel.new('Car1')
+car1 = Car.new(car_model1)
+car_model2 = CarModel.new('Car2')
+car2 = Car.new(car_model2)
+drag = Drag.new(60, car1, car2)
+
+
+#main logic
+car1.change_config
+car2.change_config
+drag.start
